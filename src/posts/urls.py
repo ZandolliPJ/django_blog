d1796f523ed990d6eb4b0cce929fd4c73adcf746
judith <judithpascal07@gmail.com>
2023-12-03 23:51:10 +0100

    CRUD

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from posts.views import BlogHome, BlogPostCreate
+from posts.views import BlogHome, BlogPostCreate, BlogPostUpdate, BlogPostDetail, BlogPostDelete
 
 
 app_name = "posts"
@@ -7,5 +7,8 @@ app_name = "posts"
 
 urlpatterns = [
     path('', BlogHome.as_view(), name='home'),
-    path('create', BlogPostCreate.as_view(), name='create')
+    path('<str:slug>/', BlogPostDetail.as_view(), name='post'),
+    path('create/', BlogPostCreate.as_view(), name='create'),
+    path('edit/<str:slug>/', BlogPostUpdate.as_view(), name='edit'),
+    path('delete/<str:slug>/', BlogPostDelete.as_view(), name='delete'),
 ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
 
 from posts.models import BlogPost
 
@@ -21,3 +22,18 @@ class BlogPostCreate(CreateView):
     model = BlogPost
     template_name = "posts/blogpost_create.html"
     fields = ['title', 'content', ]
+
+class BlogPostUpdate(UpdateView):
+    model = BlogPost
+    template_name = "posts/blogpost_edit.html"
+    fields = ['title', 'content', 'published',  ]
+
+class BlogPostDetail(DetailView):
+    model = BlogPost
+    context_object_name = "post"
+
+
+class BlogPostDelete(DeleteView):
+    model = BlogPost
+    context_object_name = "post"
+    success_url = reverse_lazy("posts:home")
